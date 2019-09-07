b95b714be29f846654358608319f66837477b725
Hoangphohieu <hoangphohieu1@gmail.com>
2019-09-07 18:31:14 +0700

    hieu

--- src/components/excelImport/ExcelImport.js
+++ src/components/excelImport/ExcelImport.js
@@ -27,17 +27,53 @@ class InputExcel extends Component {
         /* convert from workbook to array of arrays */
         var first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
         var data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 }); // data= arr[]
-        let dataState = data.shift();
-        dataState.push("printStatus");
-        dataState.push("id");
-        data = data.map(param => { return [...param, false, uuidv1()] });
-        data.unshift(dataState);
+        // let dataState = data.shift();
+        // console.log(data);
+
+        let objectConvert;
+        if (data !== []) {
+            objectConvert = data.map(param2 => {
+                let obj = {};
+                for (let j = 0; j <= param2.length - 1; j++) {
+                    (param2[j] === undefined) ? (obj[data[0][j]] = null) : (obj[data[0][j]] = param2[j]);
+                }
+                return {
+                    ...obj
+                }
+            })
+        }
+        console.log(objectConvert);
+
+        // objectConvert.shift();
+        // dataState.push("id");
+        objectConvert = objectConvert.map(param => {
+            let id = param["Name"].toUpperCase() + param["Base cost"] + _.kebabCase(param["Lineitem name"]);
+            return { ...param, id: id }
+        });
+        let listPartner = _.uniq(objectConvert.map(param => param.partner));  // lọc số partner vaf lọc trùng;
+        listPartner.shift();
+        listPartner = listPartner.map(param => { return [param, param] });
+        listPartner = _.fromPairs(listPartner);
+
+
+        let listDay = _.uniq(objectConvert.map(param => param.day));  // lọc số partner vaf lọc trùng;
+        listDay.shift();
+        listDay = listDay.map(param => { return [param, param] });
+        listDay = _.fromPairs(listDay);
+
+        // data = data.map(param => {let id=param.Name.toLowerCase()+ param[Base]; return [...param, uuidv1()] });
+        // data.unshift(dataState);
         // console.log(data);
 
 
         // dua du lieu arr[] vao local storage
         localStorage.setItem("ItemsExcel", JSON.stringify(data));
-        this.setState({ dataExcel: JSON.parse(localStorage.getItem("ItemsExcel")) })
+        this.setState({
+            dataExcel: JSON.parse(localStorage.getItem("ItemsExcel")),
+            objectDay:listDay,
+            objectPartner:listPartner
+        })
+
         // console.log(JSON.parse(localStorage.getItem("ItemsExcel")));
 
         /*end  convert from workbook to array of arrays */
@@ -75,7 +111,7 @@ class InputExcel extends Component {
             alert("Please upload a valid Excel file.");
         }
     }
-    postToServer = (param,number) => {
+    postToServer = (param, number) => {
         // console.log(param);
         let objectConvert;
         if (param !== []) {
@@ -101,14 +137,15 @@ class InputExcel extends Component {
     }
 
     render() {
+        console.log(this.state);
         let payload = this.props.itemExcelReload;
         if (payload.dataFetched === true) {
             let items = JSON.parse(localStorage.getItem("ItemsExcel"));
             if (items.length > 1) {
                 items.pop();
                 localStorage.setItem("ItemsExcel", JSON.stringify(items));
-                this.setState({ dataExcel: JSON.parse(localStorage.getItem("ItemsExcel")),numberTimeOut:this.state.numberTimeOut+100 })
-                this.postToServer(this.state.dataExcel,this.state.numberTimeOut);
+                this.setState({ dataExcel: JSON.parse(localStorage.getItem("ItemsExcel")), numberTimeOut: this.state.numberTimeOut + 100 })
+                this.postToServer(this.state.dataExcel, this.state.numberTimeOut);
             }
 
         }
